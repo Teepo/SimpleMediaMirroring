baefe837e48f7b68a0d389c182d23b39f437ea88
Anthony Ribeiro <anthony.ribeiro77@gmail.com>
2025-08-02 22:56:13 +0200

    add firebase hosting

---
+++ config/firebase.js
@@ -0,0 +1,10 @@
+const PROJECT_ID = 'simple-media-mirroring';
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyAvD--Hmll06yw9e7WAm19oZNf9GN-LtUM",
+    authDomain: `${PROJECT_ID}.firebaseapp.com`,
+    projectId: `${PROJECT_ID}`,
+    storageBucket: `${PROJECT_ID}.appspot.com`,
+    messagingSenderId: '21715672618',
+    appId: `1:21715672618:web:408598fe18c170d53e2d53`,
+};
